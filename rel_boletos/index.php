423ae9fe9e271e8afb5497addc62a174c2c0fcd4
vltscripts <160883732+vltscripts@users.noreply.github.com>
2024-03-08 10:25:16 -0400

    rel_boletos
    Melhorado Layout.

--- rel_boletos/index.php
+++ rel_boletos/index.php
@@ -9,8 +9,8 @@ if (!isset($_SESSION['MKA_Logado'])) {
 }
 
 // Assuming $Manifest is defined somewhere before this code
-$manifestTitle = isset($Manifest->{'name'}) ? $Manifest->{'name'} : '';
-$manifestVersion = isset($Manifest->{'version'}) ? $Manifest->{'version'} : '';
+$manifestTitle = $Manifest->{'name'} ?? '';
+$manifestVersion = $Manifest->{'version'} ?? '';
 ?>
 
 <!DOCTYPE html>
@@ -106,7 +106,7 @@ if (isset($_SESSION['MM_Usuario'])) {
         }
 
         table th {
-            background-color: #4caf50;
+            background-color: #0d6cea;
             color: white;
             font-weight: bold;
             text-align: center;
@@ -142,17 +142,17 @@ if (isset($_SESSION['MM_Usuario'])) {
 
         .nome_cliente td {
             border-bottom: 1px solid #ddd;
-            text-align: center; /* Adicionado para centralizar os nomes dos clientes na coluna "Nome do Cliente" */
+            text-align: center;
         }
 
         .nome_cliente:nth-child(odd) {
             background-color: #FFFF99;
         }
-		.titulo a {
-        color: #078910; /* Defina a cor laranja desejada */
-        font-weight: bold; /* Opcional: define a fonte como negrito */
-        }
 
+        .titulo a {
+            color: #078910;
+            font-weight: bold;
+        }
     </style>
 
     <script type="text/javascript">
@@ -172,8 +172,44 @@ if (isset($_SESSION['MM_Usuario'])) {
                 });
             });
         });
-    </script>
 
+        function sortTable(columnIndex) {
+            var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+            table = document.querySelector('.table-container table');
+            switching = true;
+            dir = 'asc';
+            while (switching) {
+                switching = false;
+                rows = table.rows;
+                for (i = 1; i < (rows.length - 1); i++) {
+                    shouldSwitch = false;
+                    x = rows[i].getElementsByTagName("TD")[columnIndex];
+                    y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
+                    if (dir == "asc") {
+                        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    } else if (dir == "desc") {
+                        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                }
+                if (shouldSwitch) {
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                    switchcount++;
+                } else {
+                    if (switchcount == 0 && dir == "asc") {
+                        dir = "desc";
+                        switching = true;
+                    }
+                }
+            }
+        }
+    </script>
 </head>
 
 <body>
@@ -183,7 +219,7 @@ if (isset($_SESSION['MM_Usuario'])) {
         <ul>
             <li><a href="#"> ADDON</a></li>
             <li class="is-active">
-                <a href="#" aria-current="page"> <?php echo htmlspecialchars($manifestTitle . " - V " . $manifestVersion); ?> </a>
+                <a href="#" aria-current="page"> <?= htmlspecialchars($manifestTitle . " - V " . $manifestVersion); ?> </a>
             </li>
         </ul>
     </nav>
@@ -192,17 +228,22 @@ if (isset($_SESSION['MM_Usuario'])) {
 
     <?php
     if ($acesso_permitido) {
-        // Formulário Atualizado com Funcionalidade de Busca
     ?>
         <form id="searchForm" method="GET">
-            <label for="search">Buscar Cliente:</label>
-            <input type="text" id="search" name="search" placeholder="Digite o nome do cliente, carne ou titulo" value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>">
-            <input type="submit" value="Buscar">
-            <button type="button" onclick="clearSearch()" class="clear-button">Limpar</button>
+            <div style="display: flex; justify-content: space-between; align-items: flex-end; margin-bottom: 10px;">
+                <div style="width: 60%; margin-right: 10px;">
+                    <label for="search" style="font-weight: bold; margin-bottom: 5px;">Buscar Cliente:</label>
+                    <input type="text" id="search" name="search" placeholder="Digite o Nome do Cliente" value="<?= isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>" style="width: 100%; padding: 10px; margin-bottom: 10px; border: 1px solid #ccc;">
+                </div>
+                <div style="display: flex; align-items: flex-end;">
+                    <input type="submit" value="Buscar" style="padding: 10px; border: 1px solid #4caf50; background-color: #4caf50; color: white; font-weight: bold; cursor: pointer; border-radius: 5px; margin-right: 10px;">
+                    <button type="button" onclick="clearSearch()" class="clear-button" style="padding: 10px; border: 1px solid #e74c3c; background-color: #e74c3c; color: white; font-weight: bold; cursor: pointer; border-radius: 5px; margin-right: 10px;">Limpar</button>
+                    <button type="button" onclick="sortTable(1)" class="clear-button sort-button-1" style="padding: 10px; border: 1px solid #4336f4; background-color: #4336f4; color: white; font-weight: bold; cursor: pointer; border-radius: 5px;">Ordenar</button>
+                </div>
+            </div>
         </form>
 
         <?php
-        // Dados de conexão com o banco de dados já estão em config.php
         // Consulta SQL para obter a quantidade de clientes sem carne
         $countQuery = "SELECT COUNT(c.login) AS client_count 
                FROM sis_cliente c 
@@ -213,9 +254,9 @@ if (isset($_SESSION['MM_Usuario'])) {
 
         // Verifica se há uma pesquisa por tipo de cobrança
         if (!empty($_GET['search'])) {
-        $search = '%' . mysqli_real_escape_string($link, $_GET['search']) . '%';
-        // Adiciona a condição de busca por tipo de cobrança
-        $countQuery .= " AND (c.tipo_cob LIKE ? OR c.login LIKE ? OR c.nome LIKE ?)";
+            $search = '%' . mysqli_real_escape_string($link, $_GET['search']) . '%';
+            // Adiciona a condição de busca por tipo de cobrança
+            $countQuery .= " AND (c.tipo_cob LIKE ? OR c.login LIKE ? OR c.nome LIKE ?)";
         }
 
         // Preparando a consulta
@@ -223,7 +264,7 @@ if (isset($_SESSION['MM_Usuario'])) {
 
         // Passando os parâmetros da busca, se houver
         if (!empty($_GET['search'])) {
-        mysqli_stmt_bind_param($stmt, "sss", $search, $search, $search);
+            mysqli_stmt_bind_param($stmt, "sss", $search, $search, $search);
         }
 
         // Executando a consulta
@@ -234,15 +275,15 @@ if (isset($_SESSION['MM_Usuario'])) {
 
         // Verificando se a consulta retornou resultados
         if ($countResult) {
-        // Obtendo o número de clientes sem boletos
-        $countRow = mysqli_fetch_assoc($countResult);
-        $clientCount = $countRow['client_count'];
+            // Obtendo o número de clientes sem boletos
+            $countRow = mysqli_fetch_assoc($countResult);
+            $clientCount = $countRow['client_count'];
 
-        // Exibindo o número de clientes sem boletos
-        echo "<div class='client-count-container'><p class='client-count blue'> Clientes sem Boletos: $clientCount</p></div>";
+            // Exibindo o número de clientes sem boletos
+            echo "<div class='client-count-container'><p class='client-count blue'> Clientes sem Boletos: $clientCount</p></div>";
         } else {
-        // Exibindo uma mensagem de erro caso a consulta falhe
-        echo "<div class='client-count-container'><p class='client-count blue'>Erro ao obter a quantidade de clientes</p></div>";
+            // Exibindo uma mensagem de erro caso a consulta falhe
+            echo "<div class='client-count-container'><p class='client-count blue'>Erro ao obter a quantidade de clientes</p></div>";
         }
 
 
@@ -252,43 +293,40 @@ if (isset($_SESSION['MM_Usuario'])) {
             <table>
                 <thead>
                     <tr>
-                        <th>Nome do Cliente</th>
-                        <th>Data de Pagamento</th>
-						<th>Boleto</th>
+                        <th style="border-right: 1px solid #ccc;">Nome do Cliente</th>
+                        <th style="border-right: 1px solid #ccc;">Data de Pagamento</th>
+                        <th>Boleto</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
                     // Adicione a condição de busca, se houver
-                $searchCondition = '';
-                if (!empty($_GET['search'])) {
-                $search = strtolower(mysqli_real_escape_string($link, $_GET['search'])); // Convertendo para minúsculas
-                // Verifica se a pesquisa é por "carne" ou "titulo"
-                if ($search === 'carne') {
-                $searchCondition = " AND LOWER(c.tipo_cob) LIKE 'carne'";
-                } elseif ($search === 'titulo') {
-                $searchCondition = " AND LOWER(c.tipo_cob) LIKE 'titulo'";
-                } else {
-                // Se não for "carne" nem "titulo", pesquisa por nome ou login
-                $searchCondition = " AND (LOWER(c.login) LIKE '%$search%' OR LOWER(c.nome) LIKE '%$search%')";
-                }
-                }
-
-
+                    $searchCondition = '';
+                    if (!empty($_GET['search'])) {
+                        $search = strtolower(mysqli_real_escape_string($link, $_GET['search'])); // Convertendo para minúsculas
+                        // Verifica se a pesquisa é por "carne" ou "titulo"
+                        if ($search === 'carne') {
+                            $searchCondition = " AND LOWER(c.tipo_cob) LIKE 'carne'";
+                        } elseif ($search === 'titulo') {
+                            $searchCondition = " AND LOWER(c.tipo_cob) LIKE 'titulo'";
+                        } else {
+                            // Se não for "carne" nem "titulo", pesquisa por nome ou login
+                            $searchCondition = " AND (LOWER(c.login) LIKE '%$search%' OR LOWER(c.nome) LIKE '%$search%')";
+                        }
+                    }
 
                     // Consulta SQL para obter os clientes sem carne
-                   $query = "SELECT c.uuid_cliente, c.nome, c.tipo_cob, MAX(l.datapag) AS datapag
-                   FROM sis_cliente c
-                   LEFT JOIN sis_lanc l ON c.login = l.login
-                   WHERE c.parc_abertas = 0 
-                   AND c.cli_ativado = 's' 
-                   AND (c.tipo_cob LIKE 'carne' OR c.tipo_cob LIKE 'titulo')
-                   AND c.isento LIKE 'nao'
-                   AND c.tit_abertos = 0"  // Adicionando a condição tit_abertos = 0
-                   . $searchCondition .
-                   " GROUP BY c.uuid_cliente, c.nome
-                   ORDER BY datapag DESC";  
- 
+                    $query = "SELECT c.uuid_cliente, c.nome, c.tipo_cob, MAX(l.datapag) AS datapag
+                              FROM sis_cliente c
+                              LEFT JOIN sis_lanc l ON c.login = l.login
+                              WHERE c.parc_abertas = 0 
+                              AND c.cli_ativado = 's' 
+                              AND (c.tipo_cob LIKE 'carne' OR c.tipo_cob LIKE 'titulo')
+                              AND c.isento LIKE 'nao'
+                              AND c.tit_abertos = 0" . $searchCondition .
+                        " GROUP BY c.uuid_cliente, c.nome
+                              ORDER BY datapag DESC";
+
                     // Execute a consulta
                     $result = mysqli_query($link, $query);
 
@@ -296,31 +334,40 @@ if (isset($_SESSION['MM_Usuario'])) {
                     if ($result) {
                         // Exiba os resultados da consulta SQL
                         $rowNumber = 0;
-                    while ($row = mysqli_fetch_assoc($result)) {
-                    $nome_por_num_titulo = "Nome do Cliente: " . $row['nome'] . " - UUID: " . $row['uuid_cliente'];
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $nome_por_num_titulo = "Nome do Cliente: " . $row['nome'] . " - UUID: " . $row['uuid_cliente'];
 
-                    // Adiciona a classe 'nome_cliente' e 'highlight' (para linhas ímpares) alternadamente
-                    $rowNumber++;
-                    $nomeClienteClass = ($rowNumber % 2 == 0) ? 'nome_cliente' : 'nome_cliente highlight';
-    
-                    // Adiciona a classe 'titulo' se o tipo_cob for 'titulo'
-                    if ($row['tipo_cob'] == 'titulo') {
-                    $nomeClienteClass .= ' titulo';
-                    $tipoBoleto = "Titulo";
-                    } else {
-                    $tipoBoleto = "Carne";
-                    }
+                            // Adiciona a classe 'nome_cliente' e 'highlight' (para linhas ímpares) alternadamente
+                            $rowNumber++;
+                            $nomeClienteClass = ($rowNumber % 2 == 0) ? 'nome_cliente' : 'nome_cliente highlight';
 
-                    // Adiciona o link apenas no campo de nome do cliente
-                    echo "<tr class='$nomeClienteClass'>";
-                    echo "<td><a href='../../cliente_det.hhvm?uuid=" . $row['uuid_cliente'] . "' target='_blank' title='VER CLIENTE: $nome_por_num_titulo'>" . $row['nome'] . "</a></td>";
-                    echo "<td><a style='text-align: center; text-decoration: none; cursor: default;'>" . ($row['datapag'] ? date('d/m/Y', strtotime($row['datapag'])) : 'N/A') . "</td>";
-                    echo "<td><a href=\"javascript:void(0);\" onclick=\"searchByTipoCob('".urlencode($tipoBoleto)."')\" style='text-align: center;'>$tipoBoleto</a></td>";
+                            // Adiciona a classe 'titulo' se o tipo_cob for 'titulo'
+                            if ($row['tipo_cob'] == 'titulo') {
+                                $nomeClienteClass .= ' titulo';
+                                $tipoBoleto = "Titulo";
+                            } else {
+                                $tipoBoleto = "Carne";
+                            }
 
-                    echo "</tr>";
-                    }
+                            // Adiciona o link apenas no campo de nome do cliente
+                            echo "<tr class='$nomeClienteClass'>";
+							
+                            // Nome do Cliente	
+							echo "<td style='border-right: 1px solid #ccc; position: relative;'>";
+                            echo "<img src='img/icon_cliente.png' alt='Ícone de Link' width='20' height='20' style='position: absolute; left: 0; top: 50%; transform: translateY(-50%);'> ";
+                            echo "<a href='../../cliente_det.hhvm?uuid=" . $row['uuid_cliente'] . "' target='_blank' title='VER CLIENTE: $nome_por_num_titulo'>" . $row['nome'] . "</a>";
+                            echo "</td>";
+							
+							// Data de Pagamento
+                            echo "<td style='border-right: 1px solid #ccc; text-align: center; position: relative; font-weight: bold; color: green;'>";
+                            echo "<img src='img/fatura.png' alt='Ícone de Valor' width='20' height='20' style='position: absolute; left: 0; top: 50%; transform: translateY(-50%);'> ";
+                            echo ($row['datapag'] ? date('d/m/Y', strtotime($row['datapag'])) : 'N/A');
+                            echo "</td>";
 
+                            echo "<td><a href=\"javascript:void(0);\" onclick=\"searchByTipoCob('" . urlencode($tipoBoleto) . "')\" style='text-align: center;'>$tipoBoleto</a></td>";
 
+                            echo "</tr>";
+                        }
                     } else {
                         // Se a consulta falhar, exiba uma mensagem de erro
                         echo "<tr><td colspan='2'>Erro na consulta: " . mysqli_error($link) . "</td></tr>";
@@ -340,12 +387,12 @@ if (isset($_SESSION['MM_Usuario'])) {
     <script src="../../menu.js.php"></script>
     <?php include('../../rodape.php'); ?>
 </body>
+
 <script type="text/javascript">
     function searchByTipoCob(tipoCob) {
         document.getElementById('search').value = tipoCob;
         document.forms['searchForm'].submit();
     }
 </script>
-
 
 </html>
